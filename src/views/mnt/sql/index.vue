990fead87ae1b0b0ec43d8ada9f4fcd929ee3be2
blueskyxi3 <343854933@qq.com>
2020-03-30 09:13:10 +0800

    sql page optimization

--- src/views/mnt/sql/index.vue
+++ src/views/mnt/sql/index.vue
@@ -4,89 +4,266 @@
     <div class="head-container">
       <div v-if="crud.props.searchToggle">
         <!-- 搜索 -->
-         <el-input v-model="query.value" clearable placeholder="输入搜索内容" style="width: 200px;" class="filter-item" @keyup.enter.native="crud.toQuery" />
-         <el-select v-model="query.type" clearable placeholder="类型" class="filter-item" style="width: 130px">
-          <el-option v-for="item in queryTypeOptions" :key="item.key" :label="item.display_name" :value="item.key" />
+        <el-input
+          v-model="query.value"
+          clearable
+          placeholder="输入搜索内容"
+          style="width: 200px;"
+          class="filter-item"
+          @keyup.enter.native="crud.toQuery"
+        />
+        <el-select
+          v-model="query.type"
+          clearable
+          placeholder="类型"
+          class="filter-item"
+          style="width: 130px"
+        >
+          <el-option
+            v-for="item in queryTypeOptions"
+            :key="item.key"
+            :label="item.display_name"
+            :value="item.key"
+          />
         </el-select>
-         <el-select v-model="query.dbId" clearable placeholder="数据库" class="filter-item" style="width: 130px">
-          <el-option v-for="item in databases" :key="item.name" :label="item.name" :value="item.id" />
+        <el-select
+          v-model="query.dbId"
+          clearable
+          placeholder="数据库"
+          class="filter-item"
+          style="width: 130px"
+        >
+          <el-option
+            v-for="item in databases"
+            :key="item.name"
+            :label="item.name"
+            :value="item.id"
+          />
         </el-select>
         <rrOperation :crud="crud" />
       </div>
       <!--如果想在工具栏加入更多按钮，可以使用插槽方式， slot = 'left' or 'right'-->
       <crudOperation :permission="permission" />
       <!--表单组件-->
-      <el-dialog :close-on-click-modal="false" :before-close="crud.cancelCU" :visible.sync="crud.status.cu > 0" :title="crud.status.title" width="500px">
-        <el-form ref="form" :model="form" :rules="rules" size="small" label-width="80px">
-          <el-form-item label="编码" v-if="crud.status.edit" >
-            <el-input v-model="form.id" style="width: 370px;" disabled />
+      <el-dialog
+        :close-on-click-modal="false"
+        :before-close="crud.cancelCU"
+        :visible.sync="crud.status.cu > 0"
+        :title="crud.status.title"
+        width="500px"
+      >
+        <el-form
+          ref="form"
+          :model="form"
+          :rules="rules"
+          size="small"
+          label-width="80px"
+        >
+          <el-form-item
+            v-if="crud.status.edit"
+            label="编码"
+          >
+            <el-input
+              v-model="form.id"
+              style="width: 370px;"
+              disabled
+            />
           </el-form-item>
-          <el-form-item label="进程名称" prop="name">
-            <el-input v-model="form.name" style="width: 370px;" />
+          <el-form-item
+            label="进程名称"
+            prop="name"
+          >
+            <el-input
+              v-model="form.name"
+              style="width: 370px;"
+            />
           </el-form-item>
-           <el-form-item label="数据库">
-             <el-select v-model="form.dbId" placeholder="请选择" style="width: 370px">
-               <el-option v-for="item in databases" :key="item.id" :value="item.id" :label="item.name"  />
-             </el-select>
+          <el-form-item label="数据库">
+            <el-select
+              v-model="form.dbId"
+              placeholder="请选择"
+              style="width: 370px"
+            >
+              <el-option
+                v-for="item in databases"
+                :key="item.id"
+                :value="item.id"
+                :label="item.name"
+              />
+            </el-select>
           </el-form-item>
-              <el-form-item label="系统名称" prop="serviceName">
-            <el-input v-model="form.serviceName" style="width: 370px;" />
+          <el-form-item
+            label="系统名称"
+            prop="serviceName"
+          >
+            <el-input
+              v-model="form.serviceName"
+              style="width: 370px;"
+            />
           </el-form-item>
-          <el-form-item label="SQL内容" prop="sqlText">
-            <el-input :rows="3" v-model="form.sqlText" type="textarea" style="width: 370px;" />
+          <el-form-item
+            label="SQL内容"
+            prop="sqlText"
+          >
+            <el-input
+              v-model="form.sqlText"
+              :rows="3"
+              type="textarea"
+              style="width: 370px;"
+            />
           </el-form-item>
-           <el-form-item label="规则" prop="rule">
-            <el-input :rows="3" v-model="form.rule" type="textarea" style="width: 370px;" />
+          <el-form-item
+            label="规则"
+            prop="rule"
+          >
+            <el-input
+              v-model="form.rule"
+              :rows="3"
+              type="textarea"
+              style="width: 370px;"
+            />
           </el-form-item>
           <el-form-item label="告警编码">
-            <el-select v-model="form.alertCode" filterable placeholder="请选择" style="width: 370px;" clearable>
+            <el-select
+              v-model="form.alertCode"
+              filterable
+              placeholder="请选择"
+              style="width: 370px;"
+              clearable
+            >
               <el-option
                 v-for="item in dict.alert_code"
                 :key="item.id"
                 :label="item.label"
-                :value="item.value" />
+                :value="item.value"
+              />
             </el-select>
           </el-form-item>
           <el-form-item label="备注">
-            <el-input :rows="3" v-model="form.remark" type="textarea" style="width: 370px;" />
+            <el-input
+              v-model="form.remark"
+              :rows="3"
+              type="textarea"
+              style="width: 370px;"
+            />
           </el-form-item>
         </el-form>
-        <div slot="footer" class="dialog-footer">
-          <el-button type="text" @click="crud.cancelCU">取消</el-button>
-          <el-button :loading="crud.cu === 2" type="primary" @click="crud.submitCU">确认</el-button>
+        <div
+          slot="footer"
+          class="dialog-footer"
+        >
+          <el-button
+            type="text"
+            @click="crud.cancelCU"
+          >取消</el-button>
+          <el-button
+            :loading="crud.cu === 2"
+            type="primary"
+            @click="crud.submitCU"
+          >确认</el-button>
         </div>
       </el-dialog>
       <!--表格渲染-->
-      <el-table ref="table" v-loading="crud.loading" :data="crud.data" size="small" style="width: 100%;" @selection-change="crud.selectionChangeHandler">
-        <el-table-column type="selection" width="55" />
-        <el-table-column v-if="columns.visible('id')" prop="id" label="编码" width="100" />
-        <el-table-column v-if="columns.visible('name')" prop="name" label="进程名称" :show-overflow-tooltip="true"/>
-         <el-table-column v-if="columns.visible('dbId')" prop="dbId" label="数据裤" :show-overflow-tooltip="true">
-            <template slot-scope="scope" >
-              <template v-for="item in databases">
-                  <span v-if="item.id == scope.row.dbId" :key="item.id">{{ item.name }}</span>
-              </template>
+      <el-table
+        ref="table"
+        v-loading="crud.loading"
+        :data="crud.data"
+        size="small"
+        style="width: 100%;"
+        @selection-change="crud.selectionChangeHandler"
+      >
+        <el-table-column
+          type="selection"
+          width="55"
+        />
+        <el-table-column
+          v-if="columns.visible('id')"
+          prop="id"
+          label="编码"
+          width="100"
+        />
+        <el-table-column
+          v-if="columns.visible('name')"
+          prop="name"
+          label="进程名称"
+          :show-overflow-tooltip="true"
+        />
+        <el-table-column
+          v-if="columns.visible('dbId')"
+          prop="dbId"
+          label="数据裤"
+          :show-overflow-tooltip="true"
+        >
+          <template slot-scope="scope">
+            <template v-for="item in databases">
+              <span
+                v-if="item.id == scope.row.dbId"
+                :key="item.id"
+              >{{ item.name }}</span>
+            </template>
           </template>
-         </el-table-column>
-         <el-table-column v-if="columns.visible('serviceName')" prop="serviceName" label="系统名称" :show-overflow-tooltip="true" />
-        <el-table-column v-if="columns.visible('sqlText')" prop="sqlText" label="SQL内容" :show-overflow-tooltip="true" width="320"/>
-        <el-table-column v-if="columns.visible('rule')" prop="rule" label="Rule" />
-        <el-table-column v-if="columns.visible('alertCode')" prop="alertCode" label="告警编码">
+        </el-table-column>
+        <el-table-column
+          v-if="columns.visible('serviceName')"
+          prop="serviceName"
+          label="系统名称"
+          :show-overflow-tooltip="true"
+        />
+        <el-table-column
+          v-if="columns.visible('sqlText')"
+          prop="sqlText"
+          label="SQL内容"
+          :show-overflow-tooltip="true"
+          width="320"
+        />
+        <el-table-column
+          v-if="columns.visible('rule')"
+          prop="rule"
+          label="Rule"
+          :show-overflow-tooltip="true"
+        />
+        <el-table-column
+          v-if="columns.visible('alertCode')"
+          prop="alertCode"
+          label="告警编码"
+        >
           <template slot-scope="scope">
             {{ dict.label.alert_code[scope.row.alertCode] }}
           </template>
         </el-table-column>
-        <el-table-column v-if="columns.visible('createTime')" prop="createTime" label="创建时间" width="160" :show-overflow-tooltip="true">
+        <el-table-column
+          v-if="columns.visible('createTime')"
+          prop="createTime"
+          label="创建时间"
+          width="160"
+          :show-overflow-tooltip="true"
+        >
           <template slot-scope="scope">
             <span>{{ parseTime(scope.row.createTime) }}</span>
           </template>
         </el-table-column>
-        <el-table-column v-if="columns.visible('updateDate')" prop="updateDate" label="执行" width="100">
+        <el-table-column
+          v-if="columns.visible('updateDate')"
+          prop="updateDate"
+          label="执行"
+          width="100"
+        >
           <template slot-scope="scope">
-               <el-button type="primary" v-permission="permission.edit"  icon="el-icon-caret-right"  @click="exeCMD(scope.row.id)" circle></el-button>
+            <el-button
+              v-permission="permission.edit"
+              type="primary"
+              icon="el-icon-caret-right"
+              circle
+              @click="exeCMD(scope.row.id)"
+            />
           </template>
         </el-table-column>
-        <el-table-column v-permission="['admin','sql:edit','sql:del']" label="操作" width="150px" align="center">
+        <el-table-column
+          v-permission="['admin','sql:edit','sql:del']"
+          label="操作"
+          width="150px"
+          align="center"
+        >
           <template slot-scope="scope">
             <udOperation
               :data="scope.row"
@@ -112,12 +289,12 @@ import pagination from '@crud/Pagination'
 
 // crud交由presenter持有
 const defaultCrud = CRUD({ title: 'SQL', url: 'api/sql', sort: 'id,desc', crudMethod: { ...crudSql }})
-const defaultForm = { id: null, name: null, dbId: null, sqlText: null, rule:null,createTime: null, remark: null }
+const defaultForm = { id: null, name: null, dbId: null, sqlText: null, rule: null, createTime: null, remark: null }
 export default {
   name: 'Sql',
   components: { pagination, crudOperation, rrOperation, udOperation },
   mixins: [presenter(defaultCrud), header(), form(defaultForm), crud()],
-  dicts: ['user_status','alert_code'],
+  dicts: ['user_status', 'alert_code'],
   data() {
     return {
       permission: {
@@ -137,9 +314,14 @@ export default {
         { key: 'name', display_name: '进程名称' },
         { key: 'sqlText', display_name: 'SQL内容' }
       ],
-      query:{},
-      databases:[]
+      query: {},
+      databases: []
     }
+  },
+  created() {
+    crudDatabase.getDatabases().then(res => {
+      this.databases = res.content
+    })
   },
   methods: {
     // 获取数据前设置好接口地址
@@ -154,25 +336,19 @@ export default {
       }
       return true
     },
-     exeCMD(id) {
+    exeCMD(id) {
       this.loading = true
-      crudSql.exec({'ids':id}).then((res) => {
+      crudSql.exec({ 'ids': id }).then((res) => {
         this.loading = false
         debugger
-        this.crud.notify(res ? res : '执行失败', res ? 'success' : 'error')
+        this.crud.notify(res || '执行失败', res ? 'success' : 'error')
       }).catch(() => {
         this.loading = false
       })
     }
-  },
-   created(){
-       crudDatabase.getDatabases().then(res => {
-        this.databases = res.content
-      })
   }
 }
 </script>
 
 <style scoped>
-
 </style>
