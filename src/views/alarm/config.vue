3553a3b4e12f8e8b43a781a6fd62ff2d48d9624d
Wade Yuan <wadeyuanwy@hotmail.com>
2020-06-20 11:51:38 +0800

    revised alarm code creation form

--- src/views/alarm/config.vue
+++ src/views/alarm/config.vue
@@ -15,26 +15,7 @@
       <!--表单组件-->
       <el-dialog :close-on-click-modal="false" :before-close="crud.cancelCU" :visible.sync="crud.status.cu > 0" :title="crud.status.title" width="500px">
         <el-form ref="form" :model="form" :rules="rules" size="small" label-width="120px">
-          <el-form-item label="Master Code">
-            <el-input v-model="form.masterCode" style="width: 100px;" />
-          </el-form-item>
-          <el-form-item label="Sub-code">
-            <el-input v-model="form.subCode" style="width: 100px;" />
-          </el-form-item>
-          <el-form-item label="Subject">
-            <el-input v-model="form.alarmSubject" style="width: 224px;" />
-          </el-form-item>
-          <el-form-item label="Severity">
-            <el-select v-model="form.severity" filterable placeholder="Set severity">
-              <el-option
-                v-for="item in dict.alarm_severity"
-                :key="item.id"
-                :label="item.label"
-                :value="item.value"
-              />
-            </el-select>
-          </el-form-item>
-          <el-form-item label="System Type">
+          <el-form-item label="System Type" prop="systemType">
             <el-select v-model="form.systemType" filterable placeholder="Select system type">
               <el-option
                 v-for="item in dict.system_type"
@@ -44,41 +25,68 @@
               />
             </el-select>
           </el-form-item>
-          <el-form-item label="Status">
+          <el-form-item label="Master Code" prop="masterCode">
+            <el-input-number v-model="form.masterCode" controls-position="right" style="width: 100px;" />
+          </el-form-item>
+          <el-form-item label="Sub-code" prop="subCode">
+            <el-input-number v-model="form.subCode" controls-position="right" style="width: 100px;" />
+          </el-form-item>
+          <el-form-item label="Subject" prop="alarmSubject">
+            <el-input v-model="form.alarmSubject" style="width: 224px;" />
+          </el-form-item>
+          <el-form-item label="Severity" prop="severity">
+            <el-select v-model="form.severity" filterable placeholder="Set severity">
+              <el-option
+                v-for="item in dict.alarm_severity"
+                :key="item.id"
+                :label="item.label"
+                :value="item.value"
+              />
+            </el-select>
+          </el-form-item>
+          <el-form-item label="Status" prop="isEnabled">
             <el-checkbox v-model="form.isEnabled">Enable/Disable</el-checkbox>
           </el-form-item>
-          <el-form-item label="Auto-clear after">
+          <el-form-item label="Auto-clear after" prop="autoClearTime">
             <el-input v-model="form.autoClearTime" style="width: 60px;" />&nbsp; minutes
           </el-form-item>
         </el-form>
         <div slot="footer" class="dialog-footer">
           <el-button type="text" @click="crud.cancelCU">Cancel</el-button>
-          <el-button :loading="crud.cu === 2" type="primary" @click="crud.submitCU">OK</el-button>
+          <el-button :loading="crud.cu === 2" type="primary" @click="crud.submitCU">Continue</el-button>
         </div>
       </el-dialog>
       <!--表格渲染-->
       <el-table ref="table" v-loading="crud.loading" :data="crud.data" size="small" style="width: 100%;" @selection-change="crud.selectionChangeHandler">
         <el-table-column type="selection" width="55" />
-        <el-table-column v-if="columns.visible('masterCode')" prop="masterCode" label="Master Code" />
-        <el-table-column v-if="columns.visible('subCode')" prop="subCode" label="Sub-code" />
-        <el-table-column v-if="columns.visible('alarmSubject')" prop="alarmSubject" label="Subject" />
-        <el-table-column v-if="columns.visible('severity')" prop="severity" label="Severity">
-          <template slot-scope="scope">
-            {{ dict.label.alarm_severity[scope.row.severity] }}
-          </template>
-        </el-table-column>
         <el-table-column v-if="columns.visible('systemType')" prop="systemType" label="System Type">
           <template slot-scope="scope">
             {{ dict.label.system_type[scope.row.systemType] }}
           </template>
         </el-table-column>
-        <el-table-column v-permission="['admin','alarmDefinition:edit','alarmDefinition:del']" label="Actions" width="150px" align="center">
+        <el-table-column v-if="columns.visible('masterCode')" prop="masterCode" label="Master Code" />
+        <el-table-column v-if="columns.visible('subCode')" prop="subCode" label="Sub-code" />
+        <el-table-column v-if="columns.visible('severity')" prop="severity" label="Severity">
           <template slot-scope="scope">
-            <udOperation
-              :data="scope.row"
-              :permission="permission"
-            />
+            {{ dict.label.alarm_severity[scope.row.severity] }}
           </template>
+        </el-table-column>
+        <el-table-column v-if="columns.visible('alarmSubject')" prop="alarmSubject" label="Subject" />
+        <el-table-column v-if="columns.visible('isEnabled')" prop="isEnabled" label="Status">
+          <template slot-scope="scope">
+            <span v-if="scope.row.isEnabled">Enabled</span>
+            <span v-else>Disabled</span>
+          </template>
+        </el-table-column>
+        <el-table-column v-permission="['admin','alarmDefinition:edit','alarmDefinition:del']" label="Actions" width="150px" align="center">
+          <el-button
+            icon="el-icon-setting"
+            size="mini"
+          />
+          <el-button
+            icon="el-icon-message"
+            size="mini"
+          />
         </el-table-column>
       </el-table>
       <!--分页组件-->
@@ -92,18 +100,32 @@ import crudAlarmDefinition from '@/api/alarm/alarmDefinition'
 import CRUD, { presenter, header, form, crud } from '@crud/crud'
 import rrOperation from '@crud/RR.operation'
 import crudOperation from '@crud/CRUD.operation'
-import udOperation from '@crud/UD.operation'
 import pagination from '@crud/Pagination'
+import { getByAlarmCode } from '@/api/alarm/alarmDefinition'
 
 // crud交由presenter持有
 const defaultCrud = CRUD({ title: 'alarm', url: 'alarm-api/alarmDefinition', sort: 'id,desc', crudMethod: { ...crudAlarmDefinition }})
 const defaultForm = { id: null, masterCode: null, subCode: null, alarmSubject: null, severity: null, systemType: null, alarmTemplateId: null, isEnabled: null, autoClearTime: null }
 export default {
   name: 'AlarmDefinition',
-  components: { pagination, crudOperation, rrOperation, udOperation },
+  components: { pagination, crudOperation, rrOperation },
   mixins: [presenter(defaultCrud), header(), form(defaultForm), crud()],
   dicts: ['alarm_severity', 'system_type'],
   data() {
+    const validateAlarmCode = (rule, value, callback) => {
+      if (value === 0 || value === '' || value === undefined || value == null) {
+        callback(new Error('Please input a valid alarm code'))
+      } else {
+        // Check whether the alarm code exists in system
+        getByAlarmCode(this.form.masterCode, this.form.subCode).then(result => {
+          if (result) {
+            callback(new Error('Alarm code already exists'))
+          } else {
+            callback()
+          }
+        })
+      }
+    }
     return {
       permission: {
         add: ['admin', 'alarmDefinition:add'],
@@ -111,10 +133,26 @@ export default {
         del: ['admin', 'alarmDefinition:del']
       },
       rules: {
+        systemType: [
+          { required: true, message: 'Please select a system type', trigger: 'blur' }
+        ],
+        masterCode: [
+          { required: true, trigger: 'blur', validator: validateAlarmCode }
+        ],
+        subCode: [
+          { required: true, trigger: 'blur', validator: validateAlarmCode }
+        ],
+        alarmSubject: [
+          { required: true, message: 'Please input alarm subject', trigger: 'blur' },
+          { min: 2, max: 200, message: '2 ~ 200 characters allowed', trigger: 'blur' }
+        ],
+        severity: [
+          { required: true, message: 'Please set the severity', trigger: 'blur' }
+        ]
       },
       queryTypeOptions: [
-        { key: 'severity', display_name: 'severity' },
-        { key: 'systemType', display_name: 'systemType' }
+        { key: 'severity', display_name: 'Severity' },
+        { key: 'systemType', display_name: 'System Type' }
       ]
     }
   },
@@ -126,6 +164,10 @@ export default {
         this.crud.params[query.type] = query.value
       }
       return true
+    },
+    [CRUD.HOOK.afterSubmit]() {
+      console.log('Submit complete')
+      debugger
     }
   }
 }
